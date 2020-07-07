8b30d0cabff2f132132973cf45108fb494f38c98
melanie <melanieduah@gmail.com>
2020-07-07 17:31:23 -0500

    use  db4free

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,28 @@
+// const mysql = require("mysql2/promise");
+
+// exports.connect = async () => {
+
+//     const connection = await mysql.createConnection({
+//         host: "localhost",
+//         port: 3306,
+//         user: "root",
+//         password: "root",
+//         database: "burgers_db"
+//     });
+//     return connection;
+// }
+
+
 const mysql = require("mysql2/promise");
 
 exports.connect = async () => {
 
     const connection = await mysql.createConnection({
-        host: "localhost",
+        host: "db4free.net",
         port: 3306,
-        user: "root",
-        password: "root",
+        user: "root28",
+        password: "pd$M$g2QijUGvK!",
         database: "burgers_db"
     });
     return connection;
 }
-
-
